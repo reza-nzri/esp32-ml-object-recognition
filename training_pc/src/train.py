281c61c9b32d181c7e72213dc4c0f753fff4bfe8
Malte Alexander Hartmann <maltehartmann45@gmail.com>
2026-02-12 05:51:54 +0100

    using augmented batches, increase batch size

--- training_pc/src/train.py
+++ training_pc/src/train.py
@@ -1,21 +1,16 @@
 import os
 import datetime
+import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 import keras_tuner as kt
-from sklearn.model_selection import train_test_split  # Neu hinzugefügt
+from sklearn.model_selection import train_test_split
 from utils.data_processing import DataProcessor
-from utils.model_architecture import build_model, HighPerfTuner
+from utils.model_architecture import build_model, HighPerfTuner, get_augmented_batch
 from visualizations import PerformanceDashboard
 
-
 BASE_DATA_PATH = os.path.join("data", "raw", "objects")
-
-SHAPE_DIRS = {
-    "circle": "circle",
-    "oval":"oval",
-    "square": "square"
-}
-
+SHAPE_DIRS = {"circle": "circle", "oval": "oval", "square": "square"}
 
 def run_training():
     processor = DataProcessor()
@@ -25,17 +20,14 @@ def run_training():
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
-    #Z-Score
     mean = X_train.mean()
     std = X_train.std()
     X_train = (X_train - mean) / std
     X_test = (X_test - mean) / std
 
     curr_dir = os.path.dirname(os.path.abspath(__file__))
-    tuner_dir = os.path.abspath(os.path.join(curr_dir, "..", "tuning_results"))
-
-    log_base_dir = os.path.abspath(os.path.join(curr_dir, "..", "src/logs", "fit"))
-    log_dir = os.path.join(log_base_dir, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    tuner_dir = os.path.join(curr_dir, "..", "tuning_results")
+    log_dir = os.path.join(curr_dir, "..", "src/logs", "fit", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
     tuner = HighPerfTuner(
         oracle=kt.oracles.BayesianOptimizationOracle(
@@ -47,34 +39,33 @@ def run_training():
         project_name='ultrasonic_shape_v2'
     )
 
-    tuner.search(X_train, y_train, epochs=100, batch_size=32)
+    tuner.search(X_train, y_train, epochs=50, batch_size=32)
 
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-    print(f"Beste Parameter gefunden! Lernrate: {best_hps.get('lr')}")
-
     best_model = tuner.hypermodel.build(best_hps)
 
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(
-        log_dir=log_dir,
-        histogram_freq=1,
-        update_freq='epoch'
+    callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
+        tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=15,
+            restore_best_weights=True
+        )
+    ]
+    train_dataset = tf.data.Dataset.from_generator(
+        lambda: get_augmented_batch(X_train, y_train, batch_size=256),
+        output_signature=(
+            tf.TensorSpec(shape=(32, 163, 1), dtype=tf.float32),
+            tf.TensorSpec(shape=(32,), dtype=tf.int32)
+        )
     )
-
-    best_model.fit(
-        X_train, y_train,
-        epochs=100,
-        validation_data=(X_test, y_test),
-        callbacks=[tensorboard_callback],
-        verbose=1
-    )
-
-    from visualizations import PerformanceDashboard
 
     history = best_model.fit(
-        X_train, y_train,
-        epochs=100,
+        train_dataset,
+        steps_per_epoch=len(X_train) // 32 * 10,
+        epochs=500,
         validation_data=(X_test, y_test),
-        callbacks=[tensorboard_callback],
+        callbacks=callbacks,
         verbose=1
     )
 
